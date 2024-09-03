596a37c8746409d1dd32ce59b621d4a1eb98e77c
Ani no <bryanlomerioanino@gmail.com>
2024-09-03 19:26:00 +0800

    fixed the error

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import './cards.css';
 
 function Cards() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
 import Cards from './components/Cards.jsx'
 
